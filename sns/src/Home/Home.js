ceb0d5c1dfda2707438e98ddd8798100ae84a9c2
wdixon2186 <53952397+wdixon2186@users.noreply.github.com>
2020-02-07 16:32:40 -0700

    started podcast site

---
+++ sns/src/Home/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../App.css';
+
+function Home() {
+  return (
+      <div className="section">
+    <div className="Home">
+        <h3>The Story</h3>
+    <p className="us">There are many beautiful things in this world,
+    separated best friends not being one of them.
+    In the fateful Summer of 2017, he who we know
+    as The Snob moved away in order to pursue a career
+    in film thus abandoning the one true love he may ever
+    know, The Slob. After months of bickering, bantering,
+    and fairy tale flirting over texts and messages, it’s
+    time to bring the incoherent arguments front and center.
+    Each week, we plan on discussing what it is that we believe
+    needs to be said about film at that given time with an
+    emphasis on new releases. The hope is that you walk away
+    not only learning something new about movies each week, but
+    also learning a little something about yourself along the
+    way. We hope you join us for what is sure to be an hour and
+    a half worth of groans and ughs upon hearing our diagnosis
+    on the relevant topics each given week.</p>
+    </div>
+    </div>
+  );
+}
+
+export default Home;
